c24563f2bbd932b3d308303754141c767ec26560
Ritchie Cai <ritchiecai@r2labs.org>
2012-07-17 19:21:36 -0700

    update functions to export

--- pkgdcl.lisp
+++ pkgdcl.lisp
@@ -5,7 +5,7 @@
         #:html-parse)
   
   (:export #:download
-           #:get-all-images-with-resolution))
+           #:batch-download))
 
 (declaim (optimize (safety 3)) (optimize (debug 3)) (optimize (speed 0)))
 (declaim (inline get-image-id get-href get-resolution prompt-read))

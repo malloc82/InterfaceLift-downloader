4d13339e6bc60077554288b22149657670df848c
Ritchie Cai <ritchie_cai@baylor.edu>
2018-08-24 15:21:56 -0500

    Update libraries versions

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.1.0"]
-                 [hickory "0.6.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-http "3.9.1"]
+                 [hickory "0.7.1"]]
   ;; :global-vars {*warn-on-reflection* true}
   :jvm-opts ^:replace []
   :repl-options {:port 7000})
